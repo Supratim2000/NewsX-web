57e141a5ad2fe16611953554c37fe64eafe7c0a6
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-08-09 11:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,36 @@ window.addEventListener('load', function() {
 
     let cardContainer = this.document.getElementById("card-container")
     let cardTemplate = this.document.getElementById("card-template")
+    let listItemsArray = Array.from(this.document.getElementsByClassName("nav-link-items"))
+    let navLogo = this.document.getElementById("nav-logo")
+    let searchInput = this.document.getElementById("search-input")
+    let searchButton = this.document.getElementById("search-button")
 
-    console.log("Operation started")
-    fetchData("technology")
-    console.log("Operation ended")
+    navLogo.addEventListener('click', function(event) {
+        fetchData("general")
+    })
+
+    listItemsArray.forEach((item) => {
+        console.log(item.id)
+        item.addEventListener('click', function(event) {
+            fetchData(event.target.id)
+        })
+    })
+
+    searchButton.addEventListener('click', function(event) {
+        let searchedText = searchInput.value
+        if (searchedText.value != "") {
+            searchInput.value = ""
+            fetchData(searchedText)
+        }
+    })
+
+    fetchData("general")
 
     async function fetchData(query) {
         let fetchedData = await fetch(baseUrl + query + "&apiKey=" + NEWS_API_KEY)
         let fetchedNewsItems = await fetchedData.json()
         let fetchedNewsArticles = Array.from(fetchedNewsItems.articles)
-
-        //console.log(fetchedNewsArticles)
 
         bindFetchedNewsItems(fetchedNewsArticles)
     }
@@ -23,10 +42,12 @@ window.addEventListener('load', function() {
     function bindFetchedNewsItems(fetchedNewsArticles) {
         cardContainer.innerHTML = ""
         for (let i = 0; i < fetchedNewsArticles.length; i++) {
-            //console.log(fetchedNewsArticles[i])
             if (fetchedNewsArticles[i].urlToImage != null) {
                 const clonedCard = cardTemplate.content.cloneNode(true)
                 fillFetchedArticlesIntoCard(clonedCard, fetchedNewsArticles[i])
+                clonedCard.firstElementChild.addEventListener('click', function(event) {
+                    window.open(fetchedNewsArticles[i].url)
+                })
                 cardContainer.appendChild(clonedCard)
             }
         }
